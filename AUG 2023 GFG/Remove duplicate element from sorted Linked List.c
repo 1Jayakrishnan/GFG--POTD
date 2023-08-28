a458fba5e6586ea626b0fcf302a32653e54bcbdf
1Jayakrishnan <86232928+1Jayakrishnan@users.noreply.github.com>
2023-08-28 09:43:47 +0530

    Create Remove duplicate element from sorted Linked List.c

---
+++ AUG 2023 GFG/Remove duplicate element from sorted Linked List.c
@@ -0,0 +1,15 @@
+void removeDuplicates(struct Node* head)
+{
+    struct Node* current = head;
+    
+    while (current != NULL && current->next != NULL) {
+        if (current->data == current->next->data) {
+            struct Node* temp = current->next;
+            current->next = temp->next;
+            free(temp);
+        } else {
+            current = current->next;
+        }
+    }
+    
+}
