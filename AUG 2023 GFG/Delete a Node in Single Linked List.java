cf6c08b6af6ca61279ceed9449e457964c6b247c
1Jayakrishnan <86232928+1Jayakrishnan@users.noreply.github.com>
2023-08-30 11:51:08 +0530

    Create Delete a Node in Single Linked List.java

---
+++ AUG 2023 GFG/Delete a Node in Single Linked List.java
@@ -0,0 +1,19 @@
+class GfG
+{
+    Node deleteNode(Node head, int x)
+    {
+	// Your code here
+	   if(x == 1) return head.next;
+	   
+	   Node node = head;
+	   while(node.next!=null){
+	       if(x == 2){
+	           node.next = node.next.next;
+	           break;
+	       }
+	       node = node.next;
+	       x--;
+	   }
+	   return head;
+    }
+}
