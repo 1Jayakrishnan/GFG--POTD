ca3f81d88b459ef607b7102e80b3ae7b1e8f85a0
1Jayakrishnan <86232928+1Jayakrishnan@users.noreply.github.com>
2023-10-18 17:06:48 +0530

    Create Eventual Safe States.py

---
+++ OCT2023 GFG/Eventual Safe States.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:    
+    def eventualSafeNodes(self, V : int, adj : List[List[int]]) -> List[int]:
+        # code here
+        def isSafeNode(node):
+            if visited[node] == 1:
+                return False
+            if visited[node] == 2:
+                return True
+
+            visited[node] = 1  # Mark the node as visiting.
+
+            for neighbor in adj[node]:
+                if not isSafeNode(neighbor):
+                    return False  # If any neighbor is not safe, the current node is not safe.
+
+            visited[node] = 2  # Mark the node as visited and safe.
+            return True
+
+        result = []
+        visited = [0] * V  # 0: unvisited, 1: visiting, 2: visited
+
+        for i in range(V):
+            if isSafeNode(i):
+                result.append(i)
+
+        return sorted(result)
+
